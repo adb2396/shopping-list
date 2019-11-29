ae947111dcfe4243097344c36d318f09b7c121fc
Akshay Bhamare <akshaybhamare10@gmail.com>
2019-11-29 11:59:10 +0530

    client setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Appnavbar from './components/AppNavbar';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Appnavbar />
+    </div>
+  );
+}
+
+export default App;
