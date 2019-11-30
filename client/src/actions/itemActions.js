9c6d15fd78141292b2358c15ce64e3ffcbfd8ffd
Akshay Bhamare <akshaybhamare10@gmail.com>
2019-11-30 20:16:53 +0530

    added async call to databse through actions using redux-thunk

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,25 +1,48 @@
+import axios from 'axios';
 import {
     GET_ITEMS,
     ADD_ITEMS,
-    DELETE_ITEMS
+    DELETE_ITEMS,
+    ITEMS_LOADING
 } from './types';
 
 export const getItems = () => {
-    return {
-        type: GET_ITEMS
+    return async (dispatch) => {
+        dispatch(setItemsLoading());
+        const res = await axios.get('/api/items');
+
+        dispatch({
+            type: GET_ITEMS,
+            payload: res.data
+        });
+        //console.log(res);
     };
 };
 
 export const deleteItem = (id) => {
-    return {
-        type: DELETE_ITEMS,
-        payload: id
+    return async (dispatch) => {
+        const res = await axios.delete(`/api/items/${id}`);
+
+        dispatch({
+            type: DELETE_ITEMS,
+            payload: id
+        })
     };
 };
 
 export const addItem = (item) => {
-    return {
-        type: ADD_ITEMS,
-        payload: item
+    return async (dispatch) => {
+        const res = await axios.post('/api/items', item);
+
+        dispatch({
+            type: ADD_ITEMS,
+            payload: res.data
+        })
     };
+};
+
+export const setItemsLoading = () => {
+    return {
+        type: ITEMS_LOADING
+    }
 };
